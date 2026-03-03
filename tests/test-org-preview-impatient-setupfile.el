8bf1fe6d7339b142eb2f0b15befb1e942690ab00
zhangbin <binz.hust@gmail.com>
2026-03-03 09:36:37 +0800

    test: add integration test for specific setupfile path (istyle.theme)

--- tests/test-org-preview-impatient-setupfile.el
+++ tests/test-org-preview-impatient-setupfile.el
@@ -40,3 +40,41 @@
             ;; Cleanup
             (kill-buffer org-buffer)
             (delete-directory temp-dir t)))))))
+
+(ert-deftest test-org-preview-impatient-setupfile-istyle-theme ()
+  "Test #+SETUPFILE support with specific ~/GitHub/wiki/static/themes/istyle/istyle.theme path."
+  (skip-unless (executable-find "emacs"))
+  (let* ((theme-dir (expand-file-name "~/GitHub/wiki/static/themes/istyle/"))
+         (theme-file (expand-file-name "istyle.theme" theme-dir))
+         (css-file (expand-file-name "istyle.css" theme-dir))
+         (org-buffer (generate-new-buffer "istyle-test.org"))
+         ;; We will mock the files if they do not exist
+         (mocked (not (file-exists-p theme-dir))))
+    
+    (when mocked
+      (make-directory theme-dir t)
+      (with-temp-file theme-file
+        (insert "#+HTML_HEAD: <link rel=\"stylesheet\" type=\"text/css\" href=\"istyle.css\" />\n"))
+      (with-temp-file css-file
+        (insert "body { background-color: #f0f0f0; }")))
+
+    (with-current-buffer org-buffer
+      (insert (format "#+SETUPFILE: %s\n" theme-file))
+      (insert "* Hello iStyle\n")
+      (org-mode)
+      
+      (let ((org-preview-impatient-body-only nil))
+        (let ((html (org-preview-impatient--trigger-export-sync)))
+          (unwind-protect
+              (progn
+                (should html)
+                (with-temp-buffer
+                  (insert html)
+                  (goto-char (point-min))
+                  ;; Check if the HTML_HEAD from istyle.theme is injected
+                  (should (re-search-forward "<link .*href=\"istyle\\.css\"" nil t))))
+            ;; Cleanup buffer
+            (kill-buffer org-buffer)
+            ;; Cleanup mocked files
+            (when mocked
+              (delete-directory (expand-file-name "~/GitHub/wiki") t))))))))
